34ac1b301f5b339dab5cbf01de47300b211f5c90
basileo <basileo@adsterra.com>
2020-06-22 20:07:08 +0300

    опять все переделал, на ревью

--- main.cpp
+++ main.cpp
@@ -1,24 +1,113 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+const char input[] = "input.txt";
+const char output[] = "output.txt";
+const int MAX_SIZE = 200;
 
-int main(int argc, char* argv[])
-{   struct State {
-        char marker = ' ';
-        char text[1000];
-    };
-    State state;
+struct String
+{
+public:
+    void addChar(const char & inputChar) {
+        if (inputChar == '\n') {
+            return;
+        }
 
-    cout << "Выполнил студент группы "<<endl;
-    cout << "Введите текст"<<endl;
-    cin.getline(state.text, sizeof(state.text - 1));
+        this->text[this->index] = inputChar;
+        this->index++;
+    }
+    bool isMarker(const char & inputChar) {
+        return inputChar == this->marker;
+    }
+    void saveToFile(const string& outputFileName) {
+        ofstream outputFile(outputFileName, ios_base::app);
 
-    for (int i = 0; state.text[i] != state.marker; i++)
-                if( state.text[i] <= 'z' && state.text[i] >= 'a')
-                {
-                    state.text[i] += 'A' - 'a';
-                    cout << state.text[i] << '\n';
+        if (outputFile.is_open()) {
+            int i = 0;
+
+            while (i <= this->index) {
+                outputFile << this->text[i];
+
+                if (this->isMarker(this->text[i])) {
+                    break;
                 }
-    return 0;
+
+                i++;
+            }
+
+            outputFile << endl;
+            outputFile.close();
+        }
+    };
+
+private:
+    char text[MAX_SIZE]{};
+    int index = 0;
+    char marker = '@';
+};
+
+
+int main () {
+
+    ifstream inputFile(input);
+
+    ofstream clearFile(output, ios_base::trunc);
+    clearFile.close();
+    {
+        cout << "Выполнил студент группы 9005 Басилая Андрей "<<endl;
+        //cout << "Введите текст:"<<endl;
+        String line;
+        char string[80], replace[80], found[80], str1[80], str2[80], str3[80] ;
+        cout << "\nEnter string(max 3 words)\n" ;
+        cin.getline(string , 80);
+
+        cout <<"\nВ каких слова(х) увеличить буквы \n"      ;
+        cin.getline(replace , 80);
+        for(int i=0; i<replace[0]; i++)
+            if( replace[i] <= 'z' && replace[i] >= 'a')
+            {
+                replace[i] += 'A' - 'a';
+            }
+        cout << replace;
+
+       // strreplace(string,  replace);
+        if (inputFile.is_open()) {
+            char inputChar;
+            //char n[30];
+            std::ofstream out;          // поток для записи
+            out.open("input.txt"); // окрываем файл для записи
+            if (out.is_open()) {
+//
+ //           cin.getline(n, 30);
+//                cout << n;
+//
+//                for(int i=0; i<n[i]; i++)
+//                    if( n[i] <= 'z' && n[i] >= 'a')
+//                    {
+//                        n[i] += 'A' - 'a';
+//
+//                    }
+                out << replace << std::endl;
+                out << "--------\n" << std::endl;
+                out << "Изначальный текст:" << string << std::endl;
+                out << "--------\n" << std::endl;
+
+//
+                while (inputFile  >> inputChar ) {
+
+                    line.addChar(inputChar);
+                }
+
+
+               line.addChar('@');
+                inputFile.close();
+            }
+            line.saveToFile(output);
+
+        }
+
+    }
 }
+
